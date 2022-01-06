297f6b1c581d973a213a82db005712f9ff2946d3
Garvit9000c <909garvit@gmail.com>
2022-01-07 00:46:51 +0530

    init

--- extractify.py
+++ extractify.py
@@ -9,7 +9,7 @@ path='./static/MODELS'
 
 class bert_classifier:
     def __init__(self):
-        self.model = tf.keras.models.load_model(path+'/bert_classifier')
+        self.model = tf.keras.models.load_model(path+'/bert_classifie')
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
 
 
@@ -42,7 +42,7 @@ class bert_classifier:
 
 class linking_classifier:
   def __init__(self):
-    self.model = tf.keras.models.load_model(path+'/linking')  
+    self.model = tf.keras.models.load_model(path+'/linkin')  
  
   def ext(self,box):
     width=box[2]-box[0]
