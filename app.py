6bdf3287ea42b755087a6bfd8387e7ec5781e708
Garvit9000c <909garvit@gmail.com>
2022-01-06 14:45:51 +0530

    init

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,jsonify,request
 import extractify as ext
+from pdf2image import convert_from_path
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -17,6 +18,21 @@ def api():
     ext.load()
     json = request.json
     return jsonify(ext.generate(json))
+    
+@app.route('/pdf_converter')
+def pdftoimg(request):
+    if(request.method == "POST"):
+        pdf = request.FILES['pdf_name']
+        with open('./static/tempfiles/temp.pdf', 'wb+') as destination:
+            for chunk in pdf.chunks():
+                destination.write(chunk)
+        images = convert_from_path('./static/tempfiles/temp.pdf')
+        for i in range(len(images)):
+            # Save pages as images in the pdf
+            images[i].save('./static/tempfiles/temp' +
+                           str(i) + '.jpg', 'JPEG')
+        return JsonResponse({'status': 'success'})
+    return JsonResponse({'status': 'failure'})
 
 # init
 if __name__ == '__main__':
